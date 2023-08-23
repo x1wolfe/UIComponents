68842a3db6c8a4535bf0739a36ff36f07b5eb3d1
Arcadian420 <kaclhhdo@gmail.com>
2023-08-23 18:47:03 +0200

    Added MakeDraggable

--- Components.lua
+++ Components.lua
@@ -1,3 +1,9 @@
+function MakeDraggable(parent)
+parent.Active = true
+parent.Archivable = true
+parent.Draggable = true
+end
+
 function CreateUnderLine(parent)
 
 local UnderLine = InsertService:LoadLocalAsset("rbxassetid://14543149742")
